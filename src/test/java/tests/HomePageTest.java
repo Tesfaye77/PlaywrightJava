0604d9b91f91da936eb56c8e92dae282a4350bb7
Tesfaye77 <ubogni@gmail.com>
2023-01-08 15:10:39 +0100

    9commit

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -2,6 +2,7 @@ package tests;
 
 import base.BaseTest;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import constants.AppConstants;
 import io.qameta.allure.Severity;
@@ -31,22 +32,22 @@ public class HomePageTest extends BaseTest {
 //        Assert.assertTrue(homePage.isForgotPwdLinkExist());
 //    }
 //
-//    @Test(priority = 3)
-//    @Severity(SeverityLevel.BLOCKER)
-//    public void appLoginTest() {
-//        Assert.assertTrue(homePage.doLogin(prop.getProperty("username").trim(),
-//                prop.getProperty("password").trim()));
-//                String s = homePage.getSiteLogoVision();
-//                homePage.emptyTheCart();
-//        if(s.equals("ok"))
-//            System.out.println("ok");
-//        else if (s.equals("wrong_IDs")) {
-//            Assert.fail("Informations de connexion Incorrects");
-//        } else if (s.equals("no_logo_seen")) {
-//            Assert.fail("Impossible d'acceder à la page Home");
-//        }
-//    }
-//
+    @Test(priority = 3)
+    @Severity(SeverityLevel.BLOCKER)
+    public void appLoginTest() {
+        Assert.assertTrue(homePage.doLogin(prop.getProperty("username").trim(),
+                prop.getProperty("password").trim()));
+                String s = homePage.getSiteLogoVision();
+                homePage.emptyTheCart();
+        if(s.equals("ok"))
+            System.out.println("ok");
+        else if (s.equals("wrong_IDs")) {
+            Assert.fail("Informations de connexion Incorrects");
+        } else if (s.equals("no_logo_seen")) {
+            Assert.fail("Impossible d'acceder à la page Home");
+        }
+    }
+
 //    @Test(priority = 7)
 //    public void appLogoutTest() {
 //        homePage = homePage.navigateToLoginPage();
@@ -55,11 +56,11 @@ public class HomePageTest extends BaseTest {
 //
 //
 //
-//    @Test(priority = 4)
-//    public void homePageTitleTest() {
-//        String actualTitle = homePage.getHomePageTitle();
-//        Assert.assertEquals(actualTitle, AppConstants.HOME_PAGE_TITLE);
-//    }
+    @Test(priority = 4)
+    public void homePageTitleTest() {
+        String actualTitle = homePage.getHomePageTitle();
+        Assert.assertEquals(actualTitle, AppConstants.HOME_PAGE_TITLE);
+    }
 //
 //    @Test(priority = 5)
 //    public void homePageURLTest() {
@@ -67,39 +68,39 @@ public class HomePageTest extends BaseTest {
 //        Assert.assertEquals(actualURL, prop.getProperty("url"));
 //    }
 
-//    @DataProvider
-//    public Object[][] getProductData() {
-//        return new Object[][] {
-//                { "T-shirt" },
-//                { "xoxo" },
-//                { "chaise" },
-//                { "ulrich" }
-//        };
-//    }
-//
-//    @Test(dataProvider = "getProductData",priority = 6)
-//    @Severity(SeverityLevel.BLOCKER)
-//    public void searchTest(String productName)  {
-//        homePage.doSearch(productName);
-//        Locator p = homePage.page.locator(homePage.searchResult)
-//                .filter(new Locator.FilterOptions().setHasText(productName));
-//
-//        int count = p.count();
-//        if(count==0)
-//            count++;
-//        for (int i = 0; i < count; ++i) {
-//            String s = homePage.getResultSearch(i, productName);
-//
-//        if(s.equals("ok"))
-//            System.out.println("Le produit est bien present");
-//        else if (s.equals("Aucun_produit_trouvé")) {
-//            Assert.fail("Produit inexistant dans la base de donnée");
-//        } else if (s.equals("not_ok")) {
-//            Assert.fail("Pas de correspondance entre le resulat et l'élement recherché");
-//        }
-//
-//        }
-//}
+    @DataProvider
+    public Object[][] getProductData() {
+        return new Object[][] {
+                { "T-shirt" },
+                { "xoxo" },
+                { "chaise" },
+                { "ulrich" }
+        };
+    }
+
+    @Test(dataProvider = "getProductData",priority = 6)
+    @Severity(SeverityLevel.BLOCKER)
+    public void searchTest(String productName)  {
+        homePage.doSearch(productName);
+        Locator p = homePage.page.locator(homePage.searchResult)
+                .filter(new Locator.FilterOptions().setHasText(productName));
+
+        int count = p.count();
+        if(count==0)
+            count++;
+        for (int i = 0; i < count; ++i) {
+            String s = homePage.getResultSearch(i, productName);
+
+        if(s.equals("ok"))
+            System.out.println("Le produit est bien present");
+        else if (s.equals("Aucun_produit_trouvé")) {
+            Assert.fail("Produit inexistant dans la base de donnée");
+        } else if (s.equals("not_ok")) {
+            Assert.fail("Pas de correspondance entre le resulat et l'élement recherché");
+        }
+
+        }
+}
 
 //
 //    @DataProvider
@@ -133,18 +134,18 @@ public class HomePageTest extends BaseTest {
 ////
 //    }
 
-    @DataProvider(name = "getRegistrationTestData")
-    public Object[][] getRegistrationTestData() {
-        Object usersData[][] = TestUtil.getTestData(AppConstants.CONTACTS_SHEET_NAME);
-        return usersData;
-    }
-
-
-
-    @Test(dataProvider = "getRegistrationTestData", priority = 2)
-    public void createNewUserTest(String email, String password, String passwordconf) {
-        homePage.clickRegisterButton(email, password, passwordconf);
-
-    }
+//    @DataProvider(name = "getRegistrationTestData")
+//    public Object[][] getRegistrationTestData() {
+//        Object usersData[][] = TestUtil.getTestData(AppConstants.CONTACTS_SHEET_NAME);
+//        return usersData;
+//    }
+//
+//
+//
+//    @Test(dataProvider = "getRegistrationTestData", priority = 2)
+//    public void createNewUserTest(String email, String password, String passwordconf) {
+//        homePage.clickRegisterButton(email, password, passwordconf);
+//
+//    }
 
 }

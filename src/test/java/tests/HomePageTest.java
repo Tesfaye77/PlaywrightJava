df43ac52644db0b7bc583fcef408836941619df3
Tesfaye77 <ubogni@gmail.com>
2023-01-09 12:38:38 +0100

    100

--- src/test/java/tests/HomePageTest.java
+++ src/test/java/tests/HomePageTest.java
@@ -60,11 +60,11 @@ public class HomePageTest extends BaseTest {
 //
 //
 //
-//    @Test(priority = 4)
-//    public void homePageTitleTest() {
-//        String actualTitle = homePage.getHomePageTitle();
-//        Assert.assertEquals(actualTitle, AppConstants.HOME_PAGE_TITLE);
-//    }
+    @Test(priority = 4)
+    public void homePageTitleTest() {
+        String actualTitle = homePage.getHomePageTitle();
+        Assert.assertEquals(actualTitle, AppConstants.HOME_PAGE_TITLE);
+    }
 ////
 //    @Test(priority = 5)
 //    public void homePageURLTest() {
@@ -72,71 +72,74 @@ public class HomePageTest extends BaseTest {
 //        Assert.assertEquals(actualURL, prop.getProperty("url"));
 //    }
 
-    @DataProvider
-    public Object[][] getProductData() {
-        return new Object[][] {
-                { "T-shirt" },
-                { "xoxo" },
-                { "chaise" },
-                { "ulrich" }
-        };
-    }
-
-    @Test(dataProvider = "getProductData",priority = 6)
-    @Severity(SeverityLevel.BLOCKER)
-    public void searchTest(String productName)  {
-        homePage.doSearch(productName);
-        Locator p = homePage.page.locator(homePage.searchResult)
-                .filter(new Locator.FilterOptions().setHasText(productName));
-
-        int count = p.count();
-        if(count==0)
-            count++;
-        for (int i = 0; i < count; ++i) {
-            String s = homePage.getResultSearch(i, productName);
-
-        if(s.equals("ok"))
-            System.out.println("Le produit est bien present");
-        else if (s.equals("Aucun_produit_trouvé")) {
-            Assert.fail("Produit inexistant dans la base de donnée");
-        } else if (s.equals("not_ok")) {
-            Assert.fail("Pas de correspondance entre le resulat et l'élement recherché");
-        }
-
-        }
-}
-
-//
 //    @DataProvider
-//    public Object[][] getProductDataForAdd() {
+//    public Object[][] getProductData() {
 //        return new Object[][] {
-//                { "Ampoule Vecteur Incandescent",3 },
-//                { "T-shirt en coton biologique",1 },
-//                { "Chaussures Hommes de Ville",5 }
+//                { "T-shirt" },
+//                { "xoxo" },
+//                { "chaise" },
+//                { "ulrich" }
 //        };
 //    }
-//    @Test(priority = 4,dataProvider = "getProductDataForAdd")
-//    public void addToCartTest(String productName,int X) {
-//        homePage.page.fill("id=style_input_navbar_search__Scaxy","");
-////        homePage.emptyTheCart();
-//        Boolean b = homePage.ClickOnAnArticle(productName);
-//        Assert.assertTrue(b,"Article inexistant");
-//        homePage.ClickOnAddToCart(X);
-//        Assert.assertTrue(homePage.VerifyArticleInCart(productName),"Article absent du panier");
-//        homePage.page.click("text=LES PRODUITS");
-////
-//    }
 //
-//    @Test(priority = 5,dataProvider = "getProductDataForAdd")
-//    public void suppressFromCartTest(String productName, int X) throws InterruptedException {
-////
-//        homePage.ClickOnCartIcon();
-//        for (int i=0;i<X;i++)
-//        {homePage.DeleteFromCart(productName);
+//    @Test(dataProvider = "getProductData",priority = 6)
+//    @Severity(SeverityLevel.BLOCKER)
+//    public void searchTest(String productName)  {
+//        homePage.doSearch(productName);
+//        Locator p = homePage.page.locator(homePage.searchResult)
+//                .filter(new Locator.FilterOptions().setHasText(productName));
+//
+//        int count = p.count();
+//        if(count==0)
+//            count++;
+//        for (int i = 0; i < count; ++i) {
+//            String s = homePage.getResultSearch(i, productName);
+//
+//        if(s.equals("ok"))
+//            System.out.println("Le produit est bien present");
+//        else if (s.equals("Aucun_produit_trouvé")) {
+//            Assert.fail("Produit inexistant dans la base de donnée");
+//        } else if (s.equals("not_ok")) {
+//            Assert.fail("Pas de correspondance entre le resulat et l'élement recherché");
 //        }
-//        Assert.assertFalse(homePage.VerifyArticleDeletion(productName),"Article toujours présent dans le panier");
-////
-//    }
+//
+//        }
+//}
+
+//
+    @DataProvider
+    public Object[][] getProductDataForAdd() {
+        return new Object[][] {
+                { "Ampoule Vecteur Incandescent",3 },
+                { "T-shirt en coton biologique",1 },
+                { "Chaussures Hommes de Ville",5 }
+        };
+    }
+    @Test(priority = 4,dataProvider = "getProductDataForAdd")
+    public void addToCartTest(String productName,int X) {
+        homePage.page.fill("id=style_input_navbar_search__Scaxy","");
+//        homePage.emptyTheCart();
+        Boolean b = homePage.ClickOnAnArticle(productName);
+        Assert.assertTrue(b,"Article inexistant");
+        homePage.ClickOnAddToCart(X);
+        Assert.assertTrue(homePage.VerifyArticleInCart(productName),"Article absent du panier");
+        homePage.page.click("text=LES PRODUITS");
+//
+    }
+
+    @Test(priority = 5,dataProvider = "getProductDataForAdd")
+    public void suppressFromCartTest(String productName, int X) throws InterruptedException {
+//
+        homePage.ClickOnCartIcon();
+        for (int i=0;i<X;i++)
+        {homePage.DeleteFromCart(productName);
+        }
+        Assert.assertFalse(homePage.VerifyArticleDeletion(productName),"Article toujours présent dans le panier");
+//
+    }
+
+
+
 
 //    @DataProvider(name = "getRegistrationTestData")
 //    public Object[][] getRegistrationTestData() {
